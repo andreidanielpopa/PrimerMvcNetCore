8893677d315fe8b778d270e3621953a87ce42b8b
andreidanielpopa <andreidanielpopa02@gmail.com>
2025-01-28 09:18:43 +0100

    Add project files.

---
+++ PrimerMvcNetCore/Controllers/InformacionController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimerMvcNetCore.Models;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class InformacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ControladorVista()
+        {
+            ViewBag.nombre = "Alumno";
+            ViewBag.edad = 24;
+            return View();
+        }
+
+        public IActionResult ControladorVistaModel()
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Knekro";
+            persona.Email = "knekro@email.com";
+            persona.Edad = 119;
+
+            return View( persona );
+        }
+    }
+}
